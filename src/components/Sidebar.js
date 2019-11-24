4def9a3bcf94d0b918640d19c892eb77b6f7c768
Tabrez Basha <tab22rez@gmail.com>
2019-11-24 23:34:42 +0530

    Initial commit with basic portfolio

---
+++ src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Logo from './logo.jsx';
+
+export default function SideBar({ fullMenu }) {
+  return (
+    <header id="header" className="alt">
+      <h2>T</h2>
+      <Logo />
+      <h2>Z</h2>
+    </header>
+  );
+}
